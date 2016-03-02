e305d4b1bf4a1d27686215b97d36766bce584274
chejen <jkopre.qek@gmail.com>
2016-03-02 19:55:27 +0800

    enabled reactCellRendererFactory to pass extra properties through reactComponent

--- src/ReactCellRendererFactory.ts
+++ src/ReactCellRendererFactory.ts
@@ -1,19 +1,20 @@
+/// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-export function reactCellRendererFactory(reactComponent: any): Function {
+export function reactCellRendererFactory(reactComponent: any, extraProps: any): Function {
 
     return (params: any): any => {
 
         params.eParentOfValue.addElementAttachedListener( (eCell: Element) => {
 
-            ReactDOM.render(React.createElement(reactComponent, { params: params }), eCell);
+            ReactDOM.render(React.createElement(reactComponent, Object.assign({}, extraProps, { params: params })), eCell);
 
             // if you are reading this, and want to do it using jsx, the equivalent is below.
             // however because we don't have the actual class here (just a reference to the class)
             // it can't be built into jsx. besides, the ag-grid-react-component project is so
             // small, i didn't set up jsx for it.
-            //ReactDOM.render(<SkillsCellRenderer params={params}/>, eCell);
+            //ReactDOM.render(<SkillsCellRenderer params={params} {...extraProps}/>, eCell);
 
             // we want to know when the row is taken out of the grid, so that we do React cleanup
             params.api.addVirtualRowListener('virtualRowRemoved', params.rowIndex, () => {
